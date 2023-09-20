86d26726c5c4869b0b827f1a4108f5af8af62b74
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2023-09-20 20:04:09 +0300

    fix scss

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,5 +1,13 @@
+import "../../sass/utils/_container.scss";
+import "../../sass/layouts/services.scss";
 const Services = () => {
-  return <h2>Services</h2>;
+  return (
+    <section className="services__section">
+      <div className="container">
+        <h2>Послуги</h2>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
