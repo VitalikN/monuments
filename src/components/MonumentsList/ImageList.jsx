2ed4082ae8c784c090256ed9167e6ee4eb334a50
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2023-09-24 20:22:06 +0300

    modal img

--- src/components/MonumentsList/ImageList.jsx
+++ src/components/MonumentsList/ImageList.jsx
@@ -1,19 +1,46 @@
 import Image from "next/image";
 import "../../sass/layouts/imageList.scss";
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { useEffect } from "react";
+
 export const ImageList = ({ images }) => {
+  useEffect(() => {
+    const lightbox = new SimpleLightbox(".single__list a", {
+      captionDelay: 250,
+      disableRightClick: true,
+      showCounter: false,
+      scrollZoom: false,
+    });
+
+    lightbox.on("shown.simplelightbox", () => {
+      document.body.classList.add("body-lock");
+    });
+
+    lightbox.on("close.simplelightbox", () => {
+      document.body.classList.remove("body-lock");
+    });
+    return () => {
+      lightbox.destroy(); // це вимкне попередній екземпляр lightbox перед ініціалізацією нового
+    };
+  }, [images]);
+
   return (
     <ul className="single__list">
       {images.map((img, index) => (
         <li className="single__item" key={img.id}>
-          <Image
-            className="single__img"
-            src={img.path}
-            alt="catalog/monument-accessories"
-            width="300"
-            height="400"
-            priority={true}
-          />
+          <a href={img.path}>
+            {" "}
+            <Image
+              className="single__img"
+              src={img.path}
+              alt="catalog/monument-accessories"
+              width="300"
+              height="400"
+              priority={true}
+            />
+          </a>
         </li>
       ))}
     </ul>
