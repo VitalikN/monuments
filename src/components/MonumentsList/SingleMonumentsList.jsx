9486ce6f3c9ce4c98a72b9bc6d9148f11688659c
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2023-09-24 20:32:08 +0300

    imagesPerPage

--- src/components/MonumentsList/SingleMonumentsList.jsx
+++ src/components/MonumentsList/SingleMonumentsList.jsx
@@ -8,24 +8,36 @@ import { Pagination } from "../Pagination/Pagination";
 import { ImageList } from "./ImageList";
 
 const SingleMonumentsList = () => {
-  const IMAGES_PER_PAGE = 3;
+  const [imagesPerPage, setImagesPerPage] = useState(3); // Замінити константу на стан
   const [currentPage, setCurrentPage] = useState(1);
 
   const displayedImages = singleMonument.slice(
-    (currentPage - 1) * IMAGES_PER_PAGE,
-    currentPage * IMAGES_PER_PAGE
+    (currentPage - 1) * imagesPerPage,
+    currentPage * imagesPerPage
   );
+
+  const handleImagesPerPageChange = (event) => {
+    setImagesPerPage(Number(event.target.value));
+    setCurrentPage(1); // Reset the current page to 1 when the number of images per page changes
+  };
 
   return (
     <section className="single__section">
       <div className="container">
         <h2 className="single__title">Одинарні пам`ятники</h2>
-
+        <label>
+          Зображень на сторінці:
+          <select value={imagesPerPage} onChange={handleImagesPerPageChange}>
+            <option value="3">3</option>
+            <option value="6">6</option>
+            <option value="9">9</option>
+          </select>
+        </label>
         <ImageList images={displayedImages} />
 
         <Pagination
           totalItems={singleMonument.length}
-          itemsPerPage={IMAGES_PER_PAGE}
+          itemsPerPage={imagesPerPage}
           onPageChange={setCurrentPage}
         />
       </div>
